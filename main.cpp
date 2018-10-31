e40e7259a61569eeceb9a52984ad9b510e3bb51b
splaa <splaandrey@gmail.com>
2018-10-31 19:53:18 +0100

    Операции с Целыми числами: Added Сумма цифр трехзначного числа

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 
 int main() {
-    unsigned int numberN;
+    unsigned int numberN = 0;
     std::cin >> numberN;
-    std::cout << numberN % 100 /10;
+    std::cout << numberN / 100 + numberN % 100/10 + numberN % 10;
     return 0;
 }
