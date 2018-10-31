9da8726afc32a1bcb731fe769d8fd1b7b8f1a0a2
splaa <splaandrey@gmail.com>
2018-10-31 20:05:57 +0100

    Операции с Целыми числами: Added Парты

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 
 int main() {
-    unsigned int numberN = 0;
-    std::cin >> numberN;
-    std::cout << numberN / 2*2 + 2;
+//    unsigned int numberN = 0;
+//    std::cin >> numberN;
+//    std::cout << numberN / 2*2 + 2;
     return 0;
 }
